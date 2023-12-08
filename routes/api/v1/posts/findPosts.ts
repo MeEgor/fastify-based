bba9459c09dbeb7b4c7122d3f650dffc66261384
Egor Konnov <egorknv@gmail.com>
2023-12-08 13:39:13 +0700

    немного рефакторинга

--- routes/api/v1/posts/findPosts.ts
+++ routes/api/v1/posts/findPosts.ts
@@ -2,6 +2,8 @@ import { FastifyTypeBox } from "app"
 import { findPostsResponse as response } from "./posts.schema"
 import { Comment, Post } from "../../../../models"
 // another way to use database
+// allow au to use database in any file
+// speially in services
 import { dataSource } from "../../../../plugins/db"
 
 const schema = {
@@ -12,23 +14,37 @@ const schema = {
 const postsReposittory = dataSource.getRepository(Post)
 const commentsRepository = dataSource.getRepository(Comment)
 
+function indexBy<T, K extends keyof T>(collection: T[], callback: (item: T) => K): Record<K, T> {
+  return collection.reduce((acc: Record<K, T>, item: T) => {
+    const key = callback(item)
+    acc[key] = item
+    return acc
+  }, {} as Record<K, T>)
+}
+
+export function groupBy<T, K extends keyof any>(collection: T[], callback: (item: T) => K): Record<K, T[]> {
+  return collection.reduce((acc: Record<K, T[]>, item: T) => {
+    const key = callback(item)
+    if (!acc[key]) acc[key] = []
+    acc[key].push(item)
+    return acc
+  }, {} as Record<K, T[]>)
+}
+
 // TODO: move fetching / preloading to a service
-export async function preloadComments(posts: Post[]) {
+export async function preloadComments (posts: Post[]) {
   const subQuery = posts.map(post => `(${
     commentsRepository.createQueryBuilder("comments")
-      .select("id").where(`"postId" = '${post.id}'`).limit(2)
+      .select("id")
+      .where(`"postId" = '${post.id}'`)
+      .limit(2)
       .getQuery()
   })`).join(' UNION ')
   const comments = await commentsRepository.createQueryBuilder("comments")
     .leftJoinAndSelect("comments.author", "author")
     .where(`"comments"."id" IN (${subQuery})`)
     .getMany()
-  const commentsByPostId = comments.reduce((acc: {[key: string]: Comment[]}, comment: Comment) => {
-    const { postId } = comment
-    if (!acc[postId]) acc[postId] = []
-    acc[postId].push(comment)
-    return acc
-  }, {})
+  const commentsByPostId = groupBy(comments, c => c.postId)
 
   posts.forEach(post => {
     post.comments = commentsByPostId[post.id] || []
@@ -39,6 +55,7 @@ export default async function FindPosts (fastify: FastifyTypeBox) {
   fastify.get("", { schema }, async (req, res) => {
     const posts = await postsReposittory.createQueryBuilder("posts")
       .leftJoinAndSelect("posts.author", "author")
+      .orderBy("posts.id", "DESC")
       .limit(10)
       .getMany()
     
--- routes/api/v1/users/findUsers.ts
+++ routes/api/v1/users/findUsers.ts
@@ -1,6 +1,7 @@
 import { FastifyTypeBox } from "app"
 import { findUsersResponse as response } from "./users.schema"
 import { Post } from "../../../../models"
+import { groupBy } from "../posts/findPosts"
 
 const schema = {
   description: 'Find users',
@@ -16,30 +17,22 @@ export default async function FindUsers(fastify: FastifyTypeBox) {
     const userIds = users.map(user => user.id)
 
     // Create a series of subqueries for each userId.
-    const queries = userIds.map(id => `(${
+    const subQuery = userIds.map(id => `(${
       db.posts.createQueryBuilder('post')
         .select("id")
         .where(`post."authorId" = '${id}'`)
         .limit(2)
         .getQuery()
-    })`)
-
-    // Combine all subqueries with UNION ALL.
-    const fullQuery = queries.join(' UNION ')
+    })`).join(' UNION ')
 
     // Execute the combined query with userIds as parameters.
     const posts = await db.dataSource
       .createQueryBuilder(Post, 'post')
-      .where(`post."id" IN (${fullQuery})`)
+      .where(`post."id" IN (${subQuery})`)
       .getMany()
 
     // Reduce the posts array to an object of userIds as keys and posts as values.
-    const postsByUserId = posts.reduce((acc: {[key: string]: Post[]}, post: Post) => {
-      const { authorId } = post
-      if (!acc[authorId]) acc[authorId] = []
-      acc[authorId].push(post)
-      return acc
-    }, {})
+    const postsByUserId = groupBy(posts, post => post.authorId)
     
     // Push posts to their respective users and back.
     users.forEach(user => {
